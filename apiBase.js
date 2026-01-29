75000432cf04cc7ae5b9012e12ee9e5575c7624e
Allie <allie.g.saoud@gmail.com>
2026-01-29 19:23:59 +0200

    Update apiBase.js

--- apiBase.js
+++ apiBase.js
@@ -1,5 +1,9 @@
 // Centralized API base URL for the frontend
 // Use remote API by default, fallback to local if needed
 export const API_BASE = typeof window !== 'undefined' && window.location.hostname === 'localhost'
-	? 'http://127.0.0.1:3000'
+	? 'https://abdullah-camailed-superacutely.ngrok-free.dev' //'http://127.0.0.1:3000'
 	: 'https://abdullah-camailed-superacutely.ngrok-free.dev';
+
+
+
+	
