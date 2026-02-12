a584eaba05de351bca2fe269a3a95d2080f359f1
Allie <allie.g.saoud@gmail.com>
2026-02-12 19:45:17 +0200

    fix(config): typo

--- apiBase.js
+++ apiBase.js
@@ -2,4 +2,4 @@
 // Use local API if LOCAL_DEPLOYMNT is true, otherwise use remote
 export const API_BASE = process.env.NEXT_PUBLIC_LOCAL_DEPLOYMNT === 'true'
 	? 'http://127.0.0.1:3000'
-	: 'https://wavy.dev.wavelink.com';
+	: 'https://wavy.dev.wavelink.co';
